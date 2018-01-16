bad20a920391aa04afce958b1b29ccea0ad53c4d
anand <ananddeb23@gmail.com>
2018-01-16 22:43:04 +0530

    checksome fixed with all test cases

--- checksome.js
+++ checksome.js
@@ -1,14 +1,14 @@
-ar = [];
+//ar = [];
 
-function checkIfPresent(elem){
-  console.log(elem);
-  return (ar.indexOf(elem.id)!= -1);
-}
+
 function checkUsersValid(goodUsers) {
       return function allUsersValid(submittedUsers) {
 
-
-          ar = goodUsers.map(function(ele){
+        function checkIfPresent(elem){
+          console.log(elem);
+          return (ar.indexOf(elem.id)!= -1);
+        }
+        let ar = goodUsers.map(function(ele){
             return ele.id;
           });
           return submittedUsers.every(checkIfPresent);
@@ -28,5 +28,26 @@ function checkUsersValid(goodUsers) {
       { id: 2 },
       { id: 3 }
     ];
+    //write test case for empty goodUSer and EmptyUSer
 var testAllValid = checkUsersValid(goodUsers)( submittedUsers);
+console.log(testAllValid === true);
+var testAllValid = checkUsersValid([])( submittedUsers);
+console.log(testAllValid === false);
+var testAllValid = checkUsersValid(goodUsers)( []);
+console.log(testAllValid === true);
+var testAllValid = checkUsersValid([
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  {id : 4},
+  {id : 5}
+])( []);
 console.log(testAllValid);
+var testAllValid = checkUsersValid(goodUsers)( [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  {id : 6},
+  {id: 7}
+]);
+console.log(testAllValid === false);
